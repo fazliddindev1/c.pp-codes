56aaa8a52bb6467451e78f82d368c15074cfde03
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-07-16 12:41:04 +0500

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -5,6 +5,7 @@ int main(){
     int choice;
     do {
         cout << endl;
+        cout << "---Fazliddin---" << endl;
         cout << "-----Menu------" << endl;
         cout << "1.Sum of numbers from m to n" << endl;
         cout << "2.Fibonacci series" << endl;
