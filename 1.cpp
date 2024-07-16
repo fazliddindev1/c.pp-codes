7051c10faa3c5d1876df4d5ea07470083bc7c9c9
Fazliddin <149813353+fazliddindev1@users.noreply.github.com>
2024-07-16 12:40:01 +0500

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -5,7 +5,7 @@ int main() {
     int choice;
     do {
         cout << endl;
-        court << "---Fazliddin---" << endl;
+        cout << "---Fazliddin---" << endl;
         cout << "___Menu___" << endl;
         cout << "1. Hello World" << endl;
         cout << "2. Variables" << endl;
